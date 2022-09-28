c3757f82c793b7bdd0c76dde62ed53e813ad3dca
ChristianRV29 <kmylorv@gmail.com>
2022-09-28 14:09:07 -0500

    Implement a custom hook for handling login form
    Remove register button

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment, useRef } from 'react';
-import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { BackgroundShape } from '~src/components/BackgroundShape/BackgroundShape';
 import { WhiteLogo } from '~src/components/WhiteLogo/WhiteLogo';
+import { useForm } from '~src/hooks/useForm';
+
 import {
   ActionButton,
   ActionButtonText,
@@ -18,7 +20,19 @@ import {
 export const Login = () => {
   const { top } = useSafeAreaInsets();
 
-  const newUserRef = useRef<boolean>(true);
+  const {
+    email,
+    password,
+    onChange: onChangeHook,
+  } = useForm({
+    email: undefined,
+    password: undefined,
+  });
+
+  const onLogin = () => {
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
 
   return (
     <Fragment>
@@ -33,16 +47,21 @@ export const Login = () => {
               autoCapitalize="none"
               autoCorrect={false}
               keyboardType="email-address"
+              onChangeText={value => onChangeHook(value, 'email')}
+              onSubmitEditing={onLogin}
               operativeSystem={Platform.OS}
               placeholder="Enter your email"
               placeholderTextColor="rgba(255, 255, 255, 0.4)"
               selectionColor="white"
               underlineColorAndroid="white"
+              value={email}
             />
             <FormLabel>Password</FormLabel>
             <FormTextInput
               autoCapitalize="none"
               autoCorrect={false}
+              onChangeText={value => onChangeHook(value, 'password')}
+              onSubmitEditing={onLogin}
               operativeSystem={Platform.OS}
               placeholder="Enter your password"
               placeholderTextColor="rgba(255, 255, 255, 0.4)"
@@ -50,21 +69,9 @@ export const Login = () => {
               selectionColor="white"
               underlineColorAndroid="white"
             />
-            {newUserRef.current ? (
-              <ActionButton
-                onPress={() =>
-                  console.log('Somebody is trying to register himself')
-                }>
-                <ActionButtonText>Register</ActionButtonText>
-              </ActionButton>
-            ) : (
-              <ActionButton
-                onPress={() =>
-                  console.warn('Somebody is trying to login himself')
-                }>
-                <ActionButtonText>Login</ActionButtonText>
-              </ActionButton>
-            )}
+            <ActionButton onPress={onLogin}>
+              <ActionButtonText>Login</ActionButtonText>
+            </ActionButton>
           </FormContainer>
         </Wrapper>
       </CustomKeyboardAvoidingView>
