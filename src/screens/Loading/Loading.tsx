d7236262e7a05085fd2c9c665a211aac544fe727
ChristianRV29 <kmylorv@gmail.com>
2022-10-11 17:07:31 -0500

    Create a loading screen

--- src/screens/Loading/Loading.tsx
+++ src/screens/Loading/Loading.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/native';
+import { ActivityIndicator } from 'react-native';
 
 export const Loading = () => {
   return (
-  )
-}
+    <Wrapper>
+      <ActivityIndicator size={30} color={'#2ca3da'} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.View`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+`;
