94b036b52d4b1fcf524edf34e18b88ff7469278d
ChristianRV29 <kmylorv@gmail.com>
2022-11-09 15:30:23 -0500

    Remove eslint disable commands

--- src/screens/Loading/Loading.tsx
+++ src/screens/Loading/Loading.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable curly */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
