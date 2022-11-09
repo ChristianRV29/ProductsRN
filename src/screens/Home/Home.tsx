60e851a583b9bc5186b679744cff2ddef88f7650
ChristianRV29 <kmylorv@gmail.com>
2022-11-08 19:26:27 -0500

    Refactor screens

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,18 +1,24 @@
-import React, { FC } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, useContext } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { RootStackParamList } from '~src/navigation/StackNavigator';
+import { AuthContext } from '~src/context/auth/AuthContext';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export const Home: FC<Props> = ({ route }) => {
   const { message } = route.params;
 
+  const { logOut } = useContext(AuthContext);
+
   return (
     <View style={styles.mainWrapper}>
       <Text style={styles.message}>{message}</Text>
+      <TouchableOpacity onPress={logOut}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -27,5 +33,10 @@ const styles = StyleSheet.create({
   message: {
     color: 'white',
     fontSize: 20,
+  },
+
+  button: {
+    height: 55,
+    width: 100,
   },
 });
