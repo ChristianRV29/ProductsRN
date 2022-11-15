a3f9df6d39d873d665d9201936e9d390b51e6b24
ChristianRV29 <kmylorv@gmail.com>
2022-11-15 18:53:05 -0500

    Improve imports and exports from screens folder

--- src/navigation/ProductsNavigator.tsx
+++ src/navigation/ProductsNavigator.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Products } from '~src/screens/Products/Products';
-import { Product } from '~src/screens/Products/Product';
 
-export type ProductsStackParamsList = {
-  Products: undefined;
-  Product: undefined;
-};
+import { Product, Products } from '~src/screens';
+import { ProductsStackParamsList } from '~src/@types';
 
 const { Navigator, Screen } =
   createNativeStackNavigator<ProductsStackParamsList>();
