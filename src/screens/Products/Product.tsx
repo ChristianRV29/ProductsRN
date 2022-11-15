89f132dda1faeb30aaa5c957094be6780a284555
ChristianRV29 <kmylorv@gmail.com>
2022-11-15 18:49:06 -0500

    Create screens for new stack navigator

---
+++ src/screens/Products/Product.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Product = () => {
+  return (
+    <View>
+      <Text>Welcome to Products Screen</Text>
+    </View>
+  );
+};
