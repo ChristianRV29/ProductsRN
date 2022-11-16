0a77c96ccc0a1e9ec80b1efe1a713eea0d6640f2
ChristianRV29 <kmylorv@gmail.com>
2022-11-16 13:59:00 -0500

    Add a Flatlist for showing all products

--- src/screens/Products/Products.tsx
+++ src/screens/Products/Products.tsx
@@ -1,18 +1,53 @@
-import React, { useContext, useEffect, useMemo } from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { FlatList } from 'react-native';
+
+import styled from '@emotion/native';
 
 import { ProductsContext } from '~src/context/products/ProductContext';
 
 export const Products = () => {
   const { products } = useContext(ProductsContext);
 
-  const allProducts = useMemo(() => {
-    console.log('Products: ', products);
-  }, [products]);
-
   return (
-    <View>
-      <Text>Welcome to Products Screen</Text>
-    </View>
+    <Wrapper>
+      <HeadlineContainer>
+        <Headline>Products</Headline>
+      </HeadlineContainer>
+      <FlatList
+        data={products}
+        keyExtractor={product => product._id}
+        renderItem={({ item }) => <ProductName>{item.nombre}</ProductName>}
+      />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.View`
+  background-color: #2ca3da;
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const HeadlineContainer = styled.View`
+  align-items: center;
+  background-color: #b4b0b0;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  border: 1px solid white;
+  display: flex;
+  height: 45px;
+  justify-content: center;
+  margin-bottom: 10px;
+`;
+
+const Headline = styled.Text`
+  font-size: 20px;
+  color: white;
+`;
+
+const ProductName = styled.Text`
+  font-size: 18px;
+  color: red;
+`;
